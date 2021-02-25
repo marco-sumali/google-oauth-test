c4fb4567263f94d6943a9a5cf20767a652f46c6e
Marco Sumali <marco.sumali@2359media.com>
2021-02-25 16:15:13 +0700

    chore: persinal before deploy

--- personal/src/App.js
+++ personal/src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react';
+
 function App() {
+  const [code, setCode] = useState('')
+  const {REACT_APP_GOOGLE_CLIENT_ID} = process.env
+  const gapi = window.gapi  
+
+  const signIn = () => {
+    const auth2 = gapi.auth2.init({
+      client_id: REACT_APP_GOOGLE_CLIENT_ID,
+      cookiepolicy: 'single_host_origin',
+      scope: 'profile',
+    });
+    const googleUser = auth2.currentUser.get();
+    googleUser.grantOfflineAccess({scope: 'https://www.googleapis.com/auth/calendar', prompt: 'consent'}).then(
+      function(res){
+        console.log('Success:', res);
+        setCode(res.code)
+      },
+      function(err){
+        console.log('ERROR:', err);
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Code:</div>
+        <div>{code}</div>
+        <button onClick={signIn}>Google Sign In</button>
       </header>
     </div>
   );

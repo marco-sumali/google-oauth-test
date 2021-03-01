5f30ab466022428170fcba15ac15f0831a990c4b
Marco Sumali <marco.sumali@2359media.com>
2021-03-01 12:33:10 +0700

    chore: add server button auth

--- personal/src/App.js
+++ personal/src/App.js
@@ -24,12 +24,28 @@ function App() {
         console.log('ERROR:', err);
       });
   }
+
+  const serverSignIn = async () => {
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?
+      client_id=${REACT_APP_GOOGLE_CLIENT_ID}&
+      response_type=code&
+      state=state_parameter_passthrough_value&
+      scope=https://www.googleapis.com/auth/calendar&
+      redirect_uri=http://localhost:8080&
+      prompt=consent&
+      include_granted_scopes=true`
+
+    const res = await fetch(url, {method: 'GET'})
+    console.log('check:', res)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div>Code:</div>
         <div>{code}</div>
         <button onClick={signIn}>Google Sign In</button>
+        <button onClick={serverSignIn}>Server Google Sign In</button>
       </header>
     </div>
   );
